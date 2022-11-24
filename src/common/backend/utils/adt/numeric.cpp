cf7c73619cf0e4e9d6b4b8b2e707c4ac15324aa0
夏自豪 <xiazihao3@huawei.com>
2022-11-24 01:53:32 +0000

    numeric表达式
    Signed-off-by: 夏自豪 <xiazihao3@huawei.com>

--- src/common/backend/utils/adt/numeric.cpp
+++ src/common/backend/utils/adt/numeric.cpp
@@ -196,13 +196,6 @@ static void dump_var(const char* str, NumericVar* var);
 #define dump_var(s, v)
 #endif
 
-#define digitbuf_alloc(ndigits) ((NumericDigit*)palloc((ndigits) * sizeof(NumericDigit)))
-#define digitbuf_free(buf)  \
-    do {                    \
-        if ((buf) != NULL)  \
-            pfree_ext(buf); \
-    } while (0)
-
 #define NUMERIC_CAN_BE_SHORT(scale, weight)                                         \
     ((scale) <= NUMERIC_SHORT_DSCALE_MAX && (weight) <= NUMERIC_SHORT_WEIGHT_MAX && \
         (weight) >= NUMERIC_SHORT_WEIGHT_MIN)
@@ -565,7 +558,7 @@ Datum numeric_recv(PG_FUNCTION_ARGS)
         ereport(ERROR,
             (errcode(ERRCODE_INVALID_BINARY_REPRESENTATION), errmsg("invalid length in external \"numeric\" value")));
 
-    alloc_var(&value, len);
+    init_alloc_var(&value, len);
 
     value.weight = (int16)pq_getmsgint(buf, sizeof(int16));
     value.sign = (uint16)pq_getmsgint(buf, sizeof(uint16));
@@ -2192,7 +2185,7 @@ Datum numeric_add(PG_FUNCTION_ARGS)
     init_var_from_num(num1, &arg1);
     init_var_from_num(num2, &arg2);
 
-    init_var(&result);
+    quick_init_var(&result);
     add_var(&arg1, &arg2, &result);
 
     res = make_result(&result);
@@ -2241,7 +2234,7 @@ Datum numeric_sub(PG_FUNCTION_ARGS)
     init_var_from_num(num1, &arg1);
     init_var_from_num(num2, &arg2);
 
-    init_var(&result);
+    quick_init_var(&result);
     sub_var(&arg1, &arg2, &result);
 
     res = make_result(&result);
@@ -4124,24 +4117,8 @@ static void dump_var(const char* str, NumericVar* var)
  */
 static void alloc_var(NumericVar* var, int ndigits)
 {
-    digitbuf_free(var->buf);
-    var->buf = digitbuf_alloc(ndigits + 1);
-    var->buf[0] = 0; /* spare digit for rounding */
-    var->digits = var->buf + 1;
-    var->ndigits = ndigits;
-}
-
-/*
- * free_var() -
- *
- *	Return the digit buffer of a variable to the free pool
- */
-void free_var(NumericVar* var)
-{
-    digitbuf_free(var->buf);
-    var->buf = NULL;
-    var->digits = NULL;
-    var->sign = NUMERIC_NAN;
+    digitbuf_free(var);
+    init_alloc_var(var,  ndigits);
 }
 
 /*
@@ -4152,9 +4129,8 @@ void free_var(NumericVar* var)
  */
 static void zero_var(NumericVar* var)
 {
-    digitbuf_free(var->buf);
-    var->buf = NULL;
-    var->digits = NULL;
+    digitbuf_free(var);
+    quick_init_var(var);
     var->ndigits = 0;
     var->weight = 0;         /* by convention; doesn't really matter */
     var->sign = NUMERIC_POS; /* anything but NAN... */
@@ -4358,7 +4334,7 @@ void init_var_from_num(Numeric num, NumericVar* dest)
     dest->sign = NUMERIC_SIGN(num);
     dest->dscale = NUMERIC_DSCALE(num);
     dest->digits = NUMERIC_DIGITS(num);
-    dest->buf = NULL; /* digits array is not palloc'd */
+    dest->buf = dest->ndb;
 }
 
 /*
@@ -4378,7 +4354,7 @@ static void set_var_from_var(const NumericVar* value, NumericVar* dest)
             newbuf + 1, value->ndigits * sizeof(NumericDigit), value->digits, value->ndigits * sizeof(NumericDigit));
         securec_check(rc, "\0", "\0");
     }
-    digitbuf_free(dest->buf);
+    digitbuf_free(dest);
 
     rc = memmove_s(dest, sizeof(NumericVar), value, sizeof(NumericVar));
     securec_check(rc, "\0", "\0");
@@ -4628,10 +4604,8 @@ static char* get_str_from_var_sci(NumericVar* var, int rscale)
     else
         denom_scale = 0;
 
-    ret = memset_s(&denominator, sizeof(NumericVar), 0, sizeof(NumericVar));
-    securec_check(ret, "", "");
-    ret = memset_s(&significand, sizeof(NumericVar), 0, sizeof(NumericVar));
-    securec_check(ret, "", "");
+    init_var(&denominator);
+    init_var(&significand);
 
     power_var_int(&const_ten, exponent, &denominator, denom_scale);
     div_var(var, &denominator, &significand, rscale, true);
@@ -4719,7 +4693,7 @@ Numeric make_result(NumericVar* var)
         result->choice.n_long.n_weight = weight;
     }
 
-    MemCpy(NUMERIC_DIGITS(result), digits, n * sizeof(NumericDigit));
+    memcpy(NUMERIC_DIGITS(result), digits, n * sizeof(NumericDigit));
     Assert(NUMERIC_NDIGITS(result) == (unsigned int)(n));
 
     /* Check for overflow of int16 fields */
@@ -5254,7 +5228,7 @@ static void mul_var(NumericVar* var1, NumericVar* var2, NumericVar* result, int 
     NumericDigit* var2digits = NULL;
     NumericDigit* res_digits = NULL;
     int i, i1, i2;
-
+    int tdig[NUMERIC_LOCAL_NDIG];
     /*
      * Arrange for var1 to be the shorter of the two numbers.  This improves
      * performance because the inner multiplication loop is much simpler than
@@ -5328,7 +5302,14 @@ static void mul_var(NumericVar* var1, NumericVar* var2, NumericVar* result, int 
      * possible value divided by NBASE-1, ie, at the top of the loop it is
      * known that no dig[] entry exceeds maxdig * (NBASE-1).
      */
-    dig = (int*)palloc0(res_ndigits * sizeof(int));
+    i = res_ndigits * sizeof(int);
+    if (res_ndigits > NUMERIC_LOCAL_NMAX) {
+        dig = (int *) palloc0(i);
+    } else {
+        errno_t rc = memset_s(tdig, i, 0, i);
+        securec_check(rc, "\0", "\0");
+        dig = tdig;
+    }
     maxdig = 0;
 
     /*
@@ -5406,7 +5387,9 @@ static void mul_var(NumericVar* var1, NumericVar* var2, NumericVar* result, int 
     }
     Assert(carry == 0);
 
-    pfree_ext(dig);
+    if (dig != tdig) {
+        pfree_ext(dig);
+    }
 
     /*
      * Finally, round the result to the requested precision.
@@ -6017,7 +6000,7 @@ div_var_int(const NumericVar *var, int ival, int ival_weight,
 	}
 
 	/* Store the quotient in result */
-	digitbuf_free(result->buf);
+	digitbuf_free(result);
 	result->ndigits = res_ndigits;
 	result->buf = res_buf;
 	result->digits = res_digits;
@@ -6943,6 +6926,7 @@ static void add_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
     int var2ndigits = var2->ndigits;
     NumericDigit* var1digits = var1->digits;
     NumericDigit* var2digits = var2->digits;
+    NumericDigit tdig[NUMERIC_LOCAL_NDIG];
 
     res_weight = Max(var1->weight, var2->weight) + 1;
 
@@ -6958,7 +6942,11 @@ static void add_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
         res_ndigits = 1;
     }
 
-    res_buf = digitbuf_alloc(res_ndigits + 1);
+    if (res_ndigits > NUMERIC_LOCAL_NMAX) {
+        res_buf = digitbuf_alloc(res_ndigits + 1);
+    } else {
+        res_buf = tdig;
+    }
     res_buf[0] = 0; /* spare digit for later rounding */
     res_digits = res_buf + 1;
 
@@ -6983,10 +6971,19 @@ static void add_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
 
     Assert(carry == 0); /* else we failed to allow for carry out */
 
-    digitbuf_free(result->buf);
+    digitbuf_free(result);
     result->ndigits = res_ndigits;
-    result->buf = res_buf;
-    result->digits = res_digits;
+    if (res_buf != tdig) {
+        result->buf = res_buf;
+        result->digits = res_digits;
+    } else {
+        result->buf = result->ndb;
+        result->digits = result->buf;
+        errno_t rc = memcpy_s(result->buf, sizeof(NumericDigit) * (res_ndigits + 1),
+                            res_buf, sizeof(NumericDigit) * (res_ndigits + 1));
+        securec_check(rc, "\0", "\0");
+        result->digits ++;
+    }
     result->weight = res_weight;
     result->dscale = res_dscale;
 
@@ -7019,6 +7016,7 @@ static void sub_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
     int var2ndigits = var2->ndigits;
     NumericDigit* var1digits = var1->digits;
     NumericDigit* var2digits = var2->digits;
+    NumericDigit tdig[NUMERIC_LOCAL_NDIG];
 
     res_weight = var1->weight;
 
@@ -7034,7 +7032,11 @@ static void sub_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
         res_ndigits = 1;
     }
 
-    res_buf = digitbuf_alloc(res_ndigits + 1);
+    if (res_ndigits > NUMERIC_LOCAL_NMAX) {
+        res_buf = digitbuf_alloc(res_ndigits + 1);
+    } else {
+        res_buf = tdig;
+    }
     res_buf[0] = 0; /* spare digit for later rounding */
     res_digits = res_buf + 1;
 
@@ -7059,10 +7061,19 @@ static void sub_abs(NumericVar* var1, NumericVar* var2, NumericVar* result)
 
     Assert(borrow == 0); /* else caller gave us var1 < var2 */
 
-    digitbuf_free(result->buf);
+    digitbuf_free(result);
     result->ndigits = res_ndigits;
-    result->buf = res_buf;
-    result->digits = res_digits;
+    if (res_buf != tdig) {
+        result->buf = res_buf;
+        result->digits = res_digits;
+    } else {
+        result->buf = result->ndb;
+        result->digits = result->buf;
+        errno_t rc = memcpy_s(result->buf, sizeof(NumericDigit) * (res_ndigits + 1),
+                            res_buf, sizeof(NumericDigit) * (res_ndigits + 1));
+        securec_check(rc, "\0", "\0");
+        result->digits ++;
+    }
     result->weight = res_weight;
     result->dscale = res_dscale;
 
